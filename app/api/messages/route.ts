795afcbc4fae851aee24900baf8bda03ac352122
Harshit Bamotra <harshitbamotra.01@gmail.com>
2024-02-13 21:22:51 +0530

    fetching messages

---
+++ app/api/messages/route.ts
@@ -0,0 +1,87 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { Message } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+
+
+
+
+const MessageBatch = 15;
+
+export async function GET(
+    req: Request
+){
+    try {
+        const profile = currentProfile();
+        const {searchParams} = new URL(req.url);
+
+        const cursor = searchParams.get("cursor");
+        const channelId = searchParams.get("channelId");
+
+        if(!profile){
+            return new NextResponse("Unauthoorised", {status: 401});
+        }
+        if(!channelId){
+            return new NextResponse("channel id missing", {status: 400});
+        }
+
+        let messages: Message[] = [];
+
+        if(cursor){
+            messages = await db.message.findMany({
+                take: MessageBatch,
+                skip: 1,
+                cursor:{
+                    id: cursor
+                },
+                where:{
+                    channelId: channelId
+                },
+                include:{
+                    member:{
+                        include:{
+                            profile:true
+                        }
+                    }
+                },
+                orderBy:{
+                    createdAt: "desc"
+                }
+            })
+        }
+        else{
+            messages = await db.message.findMany({
+                take: MessageBatch,
+                where:{
+                    channelId: channelId,
+                },
+                include:{
+                    member:{
+                        include:{
+                            profile: true
+                        }
+                    }
+                },
+                orderBy:{
+                    createdAt: "desc"
+                }
+            })
+        }
+
+        let nextCursor = null;
+
+        if(messages.length === MessageBatch){
+            nextCursor = messages[MessageBatch-1].id
+        }
+
+        return NextResponse.json({
+            items: messages,
+            nextCursor: nextCursor
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return new NextResponse("Internal error", {status: 500});
+    }
+}
